ebeae2ef2dfa54f1b41528bb264dcad92f697176
MartinIbarra <martineibarra@outlook.com>
2023-04-06 14:48:25 -0300

    fin clase 20

--- AppCoder/admin.py
+++ AppCoder/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
-
+from .models import *
 # Register your models here.
+
+admin.site.register(curso)
+admin.site.register(profesor)
+admin.site.register(estudiante)
+admin.site.register(entregable)
--- AppCoder/models.py
+++ AppCoder/models.py
@@ -4,6 +4,8 @@ from django.db import models
 class curso(models.Model):
     nombre = models.CharField(max_length=50)
     comision = models.IntegerField()
+    def __str__(self):
+        return f"{self.nombre} - {self.comision}"
 
 class estudiante(models.Model):
     nombre = models.CharField(max_length=50)
@@ -15,6 +17,8 @@ class profesor(models.Model):
     apellido = models.CharField(max_length=50)
     email = models.EmailField()
     profesion = models.CharField(max_length=50)
+    def __str__(self):
+        return f"{self.nombre} {self.apellido}"
 
 class entregable(models.Model):
     nombre = models.CharField(max_length=50)
